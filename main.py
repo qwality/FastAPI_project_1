ab6cd153a80fbc4f49c2391a0c523e45851e7e3e
qwality <low.qwality@gmail.com>
2023-10-28 23:08:59 +0200

    idk

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 # from enum import Enum
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse, HTMLResponse
 # from pydantic import BaseModel
-import jinja2
+# import jinja2
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory='templates')
 
 @app.get("/")
-async def index(request: Request, name: str):
+async def index(request: Request, name: str='empty'):
     return templates.TemplateResponse('home.html', {'request': request, 'name': name})
+
+@app.get("/data")
+async def get_data():
+    data = {"message": "Toto jsou data z FastAPI!"}
+    return JSONResponse(content=data)
+
+@app.get("/button/{button_name}", response_class=HTMLResponse)
+async def button(request: Request, button_name:str='empty'):
+    context = {'request': request, 'data':{"message": f"<strong>button</strong> name is {button_name}"}}
+    print('button pressed')
+    return templates.TemplateResponse('button.html', context)
+
 
 @app.get("/{other}")
 async def index(request: Request):
     return '404'
-#poznamka
 
+#dfd
 
 
--- test_server.py
+++ test_server.py
@@ -1,3 +1,3 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/dsfad").text)
+print(requests.get("http://127.0.0.1:8000/button/").text)
