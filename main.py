e5f4907814c6767165bb806c36bfa454c6e1ef9c
qwality <low.qwality@gmail.com>
2023-10-28 20:23:20 +0200

    first_commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# from enum import Enum
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+# from pydantic import BaseModel
+import jinja2
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+@app.get("/")
+async def index(request: Request, name: str):
+    return templates.TemplateResponse('home.html', {'request': request, 'name': name})
+
+@app.get("/{other}")
+async def index(request: Request):
+    return '404'
+
+
+
