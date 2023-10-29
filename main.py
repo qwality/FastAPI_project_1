3a96d5e8254b70f34b25e3ec9c33e9b538a0e348
qwality <low.qwality@gmail.com>
2023-10-29 08:50:19 +0100

    blabla

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # from enum import Enum
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Query
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import JSONResponse, HTMLResponse
 # from pydantic import BaseModel
@@ -21,7 +21,7 @@ async def get_data():
 @app.get("/button/{button_name}", response_class=HTMLResponse)
 async def button(request: Request, button_name:str='empty'):
     context = {'request': request, 'data':{"message": f"<strong>button</strong> name is {button_name}"}}
-    print('button pressed')
+    print(request.query_params.multi_items())
     return templates.TemplateResponse('button.html', context)
 
 
